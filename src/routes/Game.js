7ac4b17fb72b480f9e72e9dffc8229c6eae402db
jubi-in <thetruthrunswild@naver.com>
2021-03-03 16:18:05 +0900

    json 작성중

--- src/routes/Game.js
+++ src/routes/Game.js
@@ -3,12 +3,35 @@ import axios from "axios";
 import "./Game.css";
 import Image from '../components/Image'
 import Quest from '../components/Quest'
+
 class Game extends React.Component {
+    state = {
+        list: []
+    }
+
+    componentDidMount() {
+        fetch('content.json')
+            .then(function (result) {
+                return result.json();
+            })
+            .then(function (json) {
+                this.setState({ list: json });
+            }.bind(this));
+    }
+
     render() {
+        const list = this.state;
+
         return (
             <div className="container">
-                <Image />
-                <Quest />
+                {list.map(content => (
+                    <div>
+                        <Image
+                            image={content.image} />
+                        <Quest 
+                            id={content.id} />
+                    </div>
+                ))}
             </div>
         );
     }
